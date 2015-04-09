cf574de7cd0ea50db88fcbb96d1403e4eec13b8b
Erickson Juang <a820224@gmail.com>
2015-04-09 19:47:31 +0800

    prototype finish

--- js/game.js
+++ js/game.js
@@ -1,203 +1,184 @@
-// 在這裡寫 js code
-// 取得 canvas 物件
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-// 外牆的起始點、框線寬度與顏色
-var startX = 10;
-var startY = 10;
-var wallLineWidth = 10;
-var wallColor = "#AAAAAA";
-var wallWidth = c.width - wallLineWidth*2;
-var wallHeight = c.height - wallLineWidth*2;
-// 計算牆的邊界
-var topBound = startX + wallLineWidth/2;
-var bottomBound = startX + wallWidth - wallLineWidth/2;
-var leftBound = startY + wallLineWidth/2;
-var rightBound = startY + wallHeight - wallLineWidth/2;
-// 畫牆
-function drawWall() {
-ctx.strokeStyle = wallColor;
-ctx.lineWidth = wallLineWidth;
-ctx.strokeRect(startX, startY, wallWidth, wallHeight);
-}
-// 畫木板
-var boardWidth = 60;
-var boardStart = Math.floor(Math.random()*(wallWidth-wallLineWidth-boardWidth)) + 15;
-// 計算木板移動的左右界限
-var boardLeftBound = leftBound;
-var boardRightBound = rightBound - boardWidth;
-// 每次木板的位移量
-var unit = 10;
-function drawBoard() {
-ctx.strokeStyle = "blue";
-ctx.beginPath();
-ctx.moveTo(boardStart, c.height - wallLineWidth);
-ctx.lineTo(boardStart+boardWidth, c.height - wallLineWidth);
-ctx.closePath();
-ctx.stroke();
-}
-// 移動木板
-function moveBoard(unit){
-ctx.fillStyle = wallColor;
-ctx.fillRect(boardStart, c.height - wallLineWidth*1.5, boardWidth, wallLineWidth);
-boardStart += unit;
-if(boardStart < boardLeftBound) {
-boardStart = boardLeftBound;
-} else if(boardStart > boardRightBound){
-boardStart = boardRightBound;
-}
-drawBoard();
-}
-// 偵測鍵盤事件
-window.onkeydown = function(event){
-switch(event.keyCode){
-case 37:  // 向左
-moveBoard(-unit);
-break;
-case 39:  // 向右
-moveBoard(unit);
-break;
-}
-};
-// 彈力球的起始點與半徑
-var ballX = 100;
-var ballY = 100;
-var ballR = 10;
-// 把牆的邊界加/減上彈力球的半徑就是球的邊界
-var ballTopBound = topBound + ballR;
-var ballBottomBound = bottomBound - ballR;
-var ballLeftBound = leftBound + ballR;
-var ballRightBound = rightBound - ballR;
-// 彈力球的速度
-var speedX = 10;
-var speedY = 20;
-// 移動球
-function moveBall(){
-// 把目前的球擦掉
-ctx.clearRect(ballX-ballR, ballY-ballR, ballR*2, ballR*2);
-// 加上位移，計算新的位置
-ballX+= speedX;
-ballY+= speedY;
-// 檢查是否有撞到左右邊界，有的話要反彈
-if(ballX <= ballLeftBound) {
-var dx = ballLeftBound - (ballX - speedX);
-ballX = ballLeftBound;
-var dy = Math.round((dx/speedX) * speedY);
-ballY = ballY - speedY + dy;
-// 反彈
-speedX = -speedX;
-} else if(ballX >= ballRightBound) {
-var dx = ballRightBound - (ballX - speedX);
-ballX = ballRightBound;
-var dy = Math.round((dx/speedX) * speedY);
-ballY = ballY - speedY + dy;
-speedX = -speedX;
-}
-// 計算是否有撞到上下邊界
-if(ballY <= ballTopBound) {
-// 有撞到上邊界，反彈
-var dy = ballTopBound - (ballY - speedY);
-ballY = ballTopBound;
-var dx = Math.round((dy/speedY) * speedX);
-ballX = ballX - speedX + dx;
-speedY = -speedY;
-} else if(ballY >= ballBottomBound) {
-// 撞到下邊界時，要檢查是不是有撞在木板上，有的話反彈
-if(ballX>=boardStart && ballX<=(boardStart+boardWidth)) {
-var dy = ballBottomBound - (ballY - speedY);
-ballY = ballBottomBound;
-var dx = Math.round((dy/speedY) * speedX);
-ballX = ballX - speedX + dx;
-speedY = -speedY;
-score += dScore;
-document.getElementById("scoreDisplay").innerHTML = score;
-} else {
-// 沒有的話就掉出界外，輸掉遊戲
-ctx.fillStyle = "red";
-ctx.font = "60px impact";
-ctx.textAlign = "center";
-ctx.fillText("你輸了！", c.width/2, c.height/2, 200);
-gameStart = false;
-}
-}
-// 根據以上計算出來的位置，重新畫彈力球
-drawBall();
-if(gameStart){
-setTimeout(moveBall, 200);
-}
-}
-// 繪製彈力球
-function drawBall() {
-ctx.fillStyle = "red";
-ctx.beginPath();
-ctx.arc(ballX, ballY, ballR, 0, 2*Math.PI);
-ctx.closePath();
-ctx.fill();
-}
-var gameStart = false;
-var score = 0;
-var dScore = 10;
-// 讓使用者設定彈力球的起始點
-document.getElementById("startXRange").onchange = function(e){
-if(gameStart) {
-return;
-}
-// 清空畫布，重新開始繪製
-ctx.clearRect(0, 0, c.width, c.height);
-// 畫外牆
-drawWall();
-ballX = document.getElementById("startXRange").value;
-ballY = 100;
-drawBall();
-};
-// 背景
-var bg = document.getElementById("bg");
-var offset = 0;
-function drawBackground(){
-var y = offset;
-var i = 0;
-while (y < c.height) {
-var x = 0;
-while (x < c.width) {
-ctx.drawImage(bg, x, y, bg.width, bg.height);
-x += bg.width;
-}
-y += bg.height;
-}
-}
-// 初始化動作
-function init(){
-// 重設分數
-score = 0;
-document.getElementById("scoreDisplay").innerHTML = score;
-// 清空畫布，重新開始繪製
-ctx.clearRect(0, 0, c.width, c.height);
-// 畫外牆
-drawWall();
-// 準備彈力球的起始點，開始繪製彈力球
-ballX = Number(document.getElementById("startXRange").value);
-ballY = 100;
-drawBall();
-if(firstLoad === false) {
-// 隨機產生木板的起始點
-boardStart = Math.floor(Math.random()*(wallWidth-wallLineWidth-boardWidth)) + 15;
-drawBoard();
-startGame();
-} else {
-firstLoad = false;
-}
-}
-function startGame(){
-gameStart = true;
-// 讓球定時移動
-setTimeout(moveBall, 200);
-}
-var firstLoad = true;
-window.onload = init;
-// 掛載 click 事件，按下restart按鈕後，重新開始遊戲
-document.getElementById("restartBtn").onclick = function(){
-// 遊戲還沒有啟動的時候才可以重新啟動
-if(gameStart === false) {
-init();
-}
-};
+$(document).ready(function() {
+    //Get Canvas Object
+    var canvas = $('#myCanvas')[0];
+    var ctx = canvas.getContext('2d');
+
+    //Set ball
+    var ball = {
+        'pos': {
+            'x': 50,
+            'y': 50
+        },
+        'r': 25,
+        'color': '#eee',
+        'speed': {
+            'x': 5,
+            'y': 5
+        }
+    };
+
+    var box = {
+        'pos': {
+            'x': 0,
+            'y': 0
+        },
+        'height': 640,
+        'width': 480,
+        'thickness': 5,
+        'color': '#AAA'
+    };
+
+    var bound = {
+        'top': box['pos']['y'] + box['thickness'] / 2,
+        'bottom': box['pos']['y'] + canvas.height - box['thickness'] / 2,
+        'left': box['pos']['x'] + box['thickness'] / 2,
+        'right': box['pos']['x'] + canvas.width - box['thickness'] / 2
+    };
+
+    var board = {
+        'width': 150,
+        'thickness': 5,
+        'pos': bound['left'],
+        'speed': 50,
+        'color': '#7F7'
+    };
+
+    window.requestAnimFrame = (function(callback) {
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+            function(callback) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
+
+    //listen to keyboard
+    $(document).keydown(function(event) {
+        switch (event.keyCode) {
+            case 39:
+                moveBoard(board['speed']);
+                break;
+            case 37:
+                moveBoard(-board['speed']);
+                break;
+        }
+    });
+
+    init();
+
+    function init() {
+        drawBox();
+        drawBoard();
+        gameStart();
+        
+    }
+
+    function gameStart()
+    {
+        setTimeout(function() {
+            var startTime = (new Date()).getTime();
+            animate(ball, canvas, ctx, startTime);
+        }, 1000);
+    }
+
+
+
+    function drawBox() {
+        //Set color
+        ctx.strokeStyle = box['color'];
+        //Set line width
+        ctx.lineWidth = box['thickness'];
+        //draw
+        //ctx.strokeRect(box['thickness'] / 2 + box['pos']['x'], box['thickness'] / 2 + box['pos']['y'], box['height'], box['width']);
+        ctx.beginPath();
+        ctx.moveTo(bound['left'], bound['bottom']);
+        ctx.lineTo(bound['left'], bound['top']);
+        ctx.lineTo(bound['right'], bound['top']);
+        ctx.lineTo(bound['right'], bound['bottom']);
+        ctx.stroke();
+    }
+
+
+    function drawBoard() {
+        ctx.strokeStyle = board['color'];
+        ctx.lineWidth = board['thickness'];
+        ctx.beginPath();
+        ctx.moveTo(board['pos'], bound['bottom']);
+        ctx.lineTo(board['pos'] + board['width'], bound['bottom']);
+        ctx.stroke();
+    }
+
+    function moveBoard(unit) {
+        //clear board
+        ctx.clearRect(bound['left'] - board['thickness'] / 2, bound['bottom'] - board['thickness'] / 2, bound['right'] + board['thickness'] / 2, bound['bottom'] + board['thickness'] / 2);
+
+        //move board
+        board['pos'] += unit;
+
+        if (board['pos'] < bound['left']) {
+            board['pos'] = bound['left'];
+        } else if (board['pos'] + board['width'] > bound['right']) {
+            board['pos'] = bound['right'] - board['width'];
+        }
+    }
+
+    function animate(graph, canvas, context, startTime) {
+        var time = (new Date()).getTime() - startTime;
+
+        //decide speed
+        if ((graph['pos']['x'] + graph['r'] + graph['speed']['x']) >= bound['right'] || (graph['pos']['x'] - graph['r']  + graph['speed']['x'] * time / 1000) <= bound['left']) {
+            graph['speed']['x'] *= -1;
+        }
+
+        if ((graph['pos']['y'] - graph['r']  + graph['speed']['y']) <= bound['top']) {
+            graph['speed']['y'] *= -1;
+        } else if ((graph['pos']['y'] + graph['r']  + graph['speed']['y']) >= bound['bottom'] && (graph['pos']['x'] + graph['r'] >= board['pos'] && graph['pos']['x'] - graph['r'] <= board['pos'] + board['width'])) {
+            graph['speed']['y'] *= -1;
+        }
+
+        
+
+        var shift = {
+            'x': graph['speed']['x'] ,
+            'y': graph['speed']['y']
+        };
+
+        //clear ball
+        context.clearRect(graph['pos']['x'] - graph['r'] - 1, graph['pos']['y'] - graph['r'] - 1, graph['r'] * 2 + 2, graph['r'] * 2 + 2);
+
+        //apply shift
+        graph['pos']['x'] += shift['x'];
+        graph['pos']['y'] += shift['y'];
+
+        drawBall(graph, context);
+        drawBoard();
+        drawBox();
+
+        //condsider Acceleration of gravity
+        /*
+        
+        if(ball['speed']['y'] > 0)
+            ball['speed']['y'] += 0.098;
+        else
+        {
+            ball['speed']['y'] += 0.009;
+        }
+        */
+       
+        //board['speed'] += 0.98;
+        //console.log(ball['pos']);
+
+        // request new frame
+        requestAnimFrame(function() {
+            animate(graph, canvas, context, startTime);
+        });
+    }
+
+    function drawBall(ball, context) {
+        ctx.fillStyle = ball['color'];
+        ctx.beginPath();
+        ctx.arc(ball['pos']['x'], ball['pos']['y'], ball['r'], 2 * Math.PI, false);
+        ctx.closePath();
+        ctx.fill();
+    }
+
+
+});
